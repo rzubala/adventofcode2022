aff2a8f7e4fd424ede81408b1081377e02e4dd84
rzubala <rzubala@gmail.com>
2022-12-04 14:27:50 +0100

    02 part2

--- src/02/solve.ts
+++ src/02/solve.ts
@@ -5,7 +5,8 @@ class Solve02 extends FileReader {
     super();
     this.readData("src/02/input.data")
       .then((data) => {
-        this.process(data.split("\n"));
+        this.process(data.split("\n"), this.calculatePoints1);
+        this.process(data.split("\n"), this.calculatePoints2);
       })
       .catch((err) => console.log(err));
   }
@@ -28,15 +29,36 @@ class Solve02 extends FileReader {
     ["Z", 3]
   ])
 
-  process = (data: string[]) => {
+   myMoveMap = new Map<string, string>([
+    ["AX", "Z"],
+    ["AY", "X"],
+    ["AZ", "Y"],
+    ["BX", "X"],
+    ["BY", "Y"],
+    ["BZ", "Z"],
+    ["CX", "Y"],
+    ["CY", "Z"],
+    ["CZ", "X"],
+  ])
+
+  getPoints = (v1: string, v2: string) => {
+    return (this.scoreMap.get(`${v1}${v2}`) || 0) + (this.pointsMap.get(`${v2}`) || 0);
+  }
+
+  calculatePoints1 = (v1: string, v2: string) => this.getPoints(v1, v2)
+
+  calculatePoints2 = (v1: string, r: string) => {
+    const v2: string = this.myMoveMap.get(`${v1}${r}`) || ""
+    return this.getPoints(v1, v2)
+  }
+
+  process = (data: string[], calc: (v1: string, v2: string) => number) => {
     let sum = 0
     data.forEach(row => {
       const data: string[] = row.split(' ')
       const v1: string = data[0]
       const v2: string = data[1]
-      const points = (this.scoreMap.get(`${v1}${v2}`) || 0) + (this.pointsMap.get(`${v2}`) || 0)
-      sum += points
-      console.log(v1, v2, points)
+      sum += calc(v1, v2)
     })
     console.log(sum)
   };
