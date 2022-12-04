c36570f4ec424bb56c5ecc4f9b13f040e52cd5dc
rzubala <rzubala@gmail.com>
2022-12-04 21:04:54 +0100

    04 part2

--- src/04/solve.ts
+++ src/04/solve.ts
@@ -6,6 +6,7 @@ class Solve04 extends FileReader {
       this.readData("src/04/input.data")
         .then((data) => {
           this.process1(data.split("\n"));
+          this.process2(data.split("\n"));
         })
         .catch((err) => console.log(err));
     }
@@ -23,6 +24,20 @@ class Solve04 extends FileReader {
         })
         console.log(sum)
       };
+
+      process2 = (data: string[]) => {
+        let sum = 0
+        data.forEach(row => {
+            const data = row.split(",")
+            const p1 = data[0].split("-").map(x => +x)
+            const p2 = data[1].split("-").map(x => +x)
+            if (p1[1] >= p2[0] && p1[0] <= p2[1]
+                || p2[0] <= p1[1] && p2[1] >= p1[0]) {
+                sum++
+            }
+        })
+        console.log(sum)
+      };      
 }
     
 new Solve04();
