12fafdf0fb0e8fb1721424d37f0c71c9ee9ca8f0
rzubala <rzubala@gmail.com>
2022-12-05 13:37:18 +0100

    01

--- utils.ts
+++
@@ -1,8 +0,0 @@
-export const gcd = (a: number, b: number): number => {
-  if (a === 0) return b;
-  return gcd(b % a, a);
-};
-
-export function lcm(a: number, b: number) {
-  return !a || !b ? 0 : Math.abs((a * b) / gcd(a, b));
-}
