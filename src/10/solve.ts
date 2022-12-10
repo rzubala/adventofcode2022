11425bd44f768f6bf43f5d01b04941cf260d3c90
rzubala <rzubala@gmail.com>
2022-12-10 12:43:43 +0100

    10 part2

--- src/10/solve.ts
+++ src/10/solve.ts
@@ -9,6 +9,14 @@ class Solve10 extends FileReader {
       })
       .catch((err) => console.log(err));
   }
+  screenW = 40
+  screenH = 6
+  screen: Array<Array<string>> = new Array(this.screenH)
+  printScreen = () => {
+    for (let i=0;i<this.screenH;i++) {
+      console.log(this.screen[i].join(''))
+    }
+  }
 
   process = (data: string[]) => {
     let pc = 0
@@ -17,30 +25,39 @@ class Solve10 extends FileReader {
     let hold = false
     let param: number | undefined = undefined
     let result = 0
-
+    
+    for (let i=0;i<this.screenH;i++) {
+      this.screen[i] = new Array(this.screenW).fill('.')
+    }
+    
     while (hold || pc < data.length) {
-        cycles+=1
-        if ((cycles - 20) % 40 === 0) {
-          result += cycles * X
+      let crtPos = cycles % 40 + 1
+      let crtRow = Math.floor(cycles / 40)
+      cycles+=1
+      if ((cycles - 20) % 40 === 0) {
+        result += cycles * X
+      }      
+      if (crtPos >= X && crtPos < X + 3) {
+        this.screen[crtRow][crtPos - 1] = "#"
+      }
+      if (hold) {
+        if (param !== undefined) {
+          X += param
         }
-  
-        if (hold) {
-            if (param !== undefined) {
-                X += param
-            }
             hold = false
-        } else {
+          } else {
             const row = data[pc++]
             const tmp = row.split(' ')
             const op = tmp[0]
             if ("addx" === op) {
-                param = parseInt(tmp[1], 10)
-                hold = true
+              param = parseInt(tmp[1], 10)
+              hold = true
             }
+          }
         }
-    }
-    console.log(result)  
-  };
+        console.log(result)  
+        this.printScreen()
+      };
 }
 
 new Solve10();
